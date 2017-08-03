3aa5fd5f4cf6c98318e995747409bfb5d0de14bc
adenekan41 <adenekanwonderful41@gmail.com>
2017-08-03 13:15:58 +0100

    added my created loader

--- js/main.js
+++ js/main.js
@@ -30,3 +30,13 @@ function topFunction() {
 // end if
 
 
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
